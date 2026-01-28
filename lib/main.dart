eb90f56f50bb278eb62037cfde277f64ee19545f
tanvirulislam <tanvirul.cse.diu@gmail.com>
2026-01-28 14:11:29 +0600

    delete files

--- lib/main.dart
+++ lib/main.dart
@@ -64,28 +64,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   final cameras = await availableCameras();
-//   runApp(MyApp(cameras: cameras));
-// }
-
-// class MyApp extends StatelessWidget {
-//   final List<CameraDescription> cameras;
-
-//   const MyApp({super.key, required this.cameras});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Face Detection Validation',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       home: Scaffold(
-//         appBar: AppBar(title: const Text('Face Detection Validation')),
-//         body: Column(children: []),
-//       ),
-//     );
-//   }
-// }
